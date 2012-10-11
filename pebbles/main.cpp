01ae159ee90f34d9e104fbbbc9952a3b35b75237
Peter Braun <peter-braun@gmx.net>
2012-10-11 22:32:37 +0200

    Decided to not implement a module loader as part of the stdlib

--- pebbles/main.cpp
+++ pebbles/main.cpp
@@ -2,13 +2,65 @@
 #include <string>
 #include <fstream>
 #include <cassert>
+#include <dlfcn.h>
 
 #include "../include/PebbleScript.h"
 #include "../include/Stdlib.h"
-#include "../include/Modules.h"
+#include "include/ModuleFinder.h"
 
 void usage() {
   std::cout << "usage: pebbles [PATH TO FILE]" << std::endl;
+}
+
+/**
+ * Allow the scripts to load external resources (shared libs)
+ */
+void require(PS::Environment *env) {
+  if (env->expect(PS::String_T)) {    
+    std::string name = env->pop<std::string>();
+    
+    ModuleFinder finder;
+    as_module_t module = finder.lookup(name);
+
+    if (module.type == As_T_Invalid) {
+      std::ostringstream ss;
+      ss << "A module with the name '";
+      ss << name;
+      ss << "' cound not be found";
+      env->raise(ss.str().c_str());
+      return;
+    }
+    
+    void *handle = dlopen(module.path.c_str(), RTLD_NOW | RTLD_GLOBAL);
+    if (!handle) {
+      std::ostringstream ss;
+      ss << "Failed to load native module '";
+      ss << name;
+      ss << "'";
+      env->raise(ss.str().c_str());
+      return;
+    } else {
+      // Reset errors
+      dlerror();
+
+      // Find the install function
+      PS::ExternalFunction installHook = (PS::ExternalFunction) dlsym(handle, "install");
+      
+      // Check for errors again
+      const char* dlsym_error = dlerror();
+      if (dlsym_error) {
+        std::cout << "error: " << dlsym_error << std::endl;
+        std::ostringstream ss;
+        ss << "Native module '";
+        ss << name;
+        ss << "' is corrupt";
+        env->raise(ss.str().c_str());
+        return;
+      } else {
+        installHook(env);
+      }
+    }
+  }
 }
 
 bool loadSource(const char *path, std::string *source, bool ignoreFirstLine=false) {
@@ -48,7 +100,8 @@ int main(int argc, char *argv[])
   if (loadSource(argv[1], &source, true)) {
     PS::VM vm;
     PS::Stdlib::install(vm);
-    PS::Modules::install(vm);
+    
+    vm.def("require", require);
     
     if (!vm.eval(source.c_str())) {
       std::cerr << vm.getError() << std::endl;
